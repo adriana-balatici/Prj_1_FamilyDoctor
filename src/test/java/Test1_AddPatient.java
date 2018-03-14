a6bb1dfe964b1c4a4d8c040f04d34b004e8d5e4f
Adriana Balatici <balaticiadriana@gmail.com>
2018-03-14 11:27:29 +0200

    lab 1

---
+++ src/test/java/Test1_AddPatient.java
@@ -0,0 +1,23 @@
+import controller.DoctorController;
+import model.Patient;
+import org.junit.jupiter.api.Test;
+import repository.Repository;
+
+import java.util.List;
+
+/**
+ * Created by Adriana on 3/14/2018.
+ */
+public class Test1_AddPatient {
+    @Test
+    public void testAddPatient() {
+        Patient p = new Patient("adriana","1111111111111","adresa");
+        String patients, consultations;
+        patients = new String();
+        consultations = new String();
+        Repository repo = new Repository(patients, consultations);;
+        List<Patient> patientList = repo.getPatientList();
+        System.out.println(patientList.size());
+
+    }
+}
